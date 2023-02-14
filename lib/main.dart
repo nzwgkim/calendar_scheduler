fb0da4724b299da9b81493d942e4c9c389b0ecbf
Woody <nz.wgkim@gmail.com>
2023-02-14 16:45:26 +1300

    done schedule cards using ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:calenar_scheduler/components/calendar.dart';
+import 'package:calenar_scheduler/components/schedule_card.dart';
 import 'package:calenar_scheduler/components/today_banner.dart';
+import 'package:calenar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -41,8 +43,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print('HomeScreen 1) sel: ${selectedDay}');
+    print('HomeScreen 1) sel: $selectedDay');
+    int count = 5;
     return Scaffold(
+      floatingActionButton: floatingButton(),
       body: Center(
         child: SafeArea(
           child: Column(
@@ -51,12 +55,35 @@ class _HomeScreenState extends State<HomeScreen> {
                   selectedDay: selectedDay,
                   focusedDay: focusedDay,
                   onDaySelected: onDaySelected),
-              TodayBanner(scheduledCount: 5, selectedDay: selectedDay),
+              TodayBanner(scheduledCount: count, selectedDay: selectedDay),
+              ScheduledList(count: count),
             ],
           ),
         ),
       ),
     );
+  }
+
+  FloatingActionButton floatingButton() {
+    return FloatingActionButton(
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => SizedBox(
+              // color: Colors.white,
+              height: 300,
+              child: Column(
+                children: [
+                  Row(
+                    children: const [],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.add));
   }
 
   onDaySelected(selectedDay, focusedDay) {
@@ -66,5 +93,21 @@ class _HomeScreenState extends State<HomeScreen> {
 
       print('HomeScreen 2) sel: ${this.selectedDay}');
     });
+  }
+}
+
+class ScheduledList extends StatelessWidget {
+  final int count;
+  const ScheduledList({required this.count, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: count,
+        itemBuilder: (context, index) => const ScheduleCard(
+            startTime: 5, endTime: 6, contents: 'contents', color: Colors.red),
+      ),
+    );
   }
 }
