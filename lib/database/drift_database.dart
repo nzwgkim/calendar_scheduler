8abb0950868eeb37333d367a542e63dbf6b923dd
Woody <nz.wgkim@gmail.com>
2023-02-16 12:02:54 +1300

    Register querys

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -22,6 +22,53 @@ part 'drift_database.g.dart';
 // naming rule: class (name) extends _$(name)
 class LocalDatabase extends _$LocalDatabase {
   LocalDatabase() : super(_openConnection());
+
+  Future<Schedule> getScheduleById(int id) =>
+      (select(schedules)..where((tbl) => tbl.id.equals(id))).getSingle();
+
+// insert Query
+// column에 접근하려면, xxxCompanion 을 통해서만 가능하다.
+  Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+  Future<int> createCategoryColor(CategoryColorsCompanion data) =>
+      into(categoryColors).insert(data);
+
+// select Query
+  Future<List<CategoryColor>> getCategoryColors() =>
+      select(categoryColors).get();
+// update Query
+  Future<int> updateScheduleById(int id, SchedulesCompanion data) =>
+      (update(schedules)..where((tbl) => tbl.id.equals(id))).write(data);
+// delete Query
+  Future<int> removeSchedule(int id) =>
+      (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
+
+  Stream<List<ScheduleWithColor>> watchSchedules(DateTime date) {
+    final query = select(schedules).join([
+      innerJoin(categoryColors, categoryColors.id.equalsExp(schedules.colorId))
+    ]);
+
+    query.where(schedules.date.equals(date));
+    query.orderBy(
+      [
+        // asc -> ascending 오름차순
+        // desc -> descending 내림차순
+        OrderingTerm.asc(schedules.startTime),
+      ],
+    );
+
+    return query.watch().map(
+          (rows) => rows
+              .map(
+                (row) => ScheduleWithColor(
+                  schedule: row.readTable(schedules),
+                  categoryColor: row.readTable(categoryColors),
+                ),
+              )
+              .toList(),
+        );
+  }
+
   @override
   int get schemaVersion => 1;
 }
